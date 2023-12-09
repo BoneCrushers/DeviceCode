af42fdc9fdec29673bcf70678feee83953c3cc83
Flint Million <fmillion@d50.lan.katogeek.com>
2023-12-09 09:39:38 +0000

    more test code for accellerometer

--- bonecrushers/cli/test.py
+++ bonecrushers/cli/test.py
@@ -5,5 +5,9 @@ def main():
     try:
         p = Positioner()
         print(f"The test was successful.")
+        print(p.temp_c)
+
+        import timeit
+        print(timeit.timeit("p.temp_c",number=50,globals={"p":p}))
     except RuntimeError as ex:
         print(f"The test was not successful: {ex}")
--- bonecrushers/library/position.py
+++ bonecrushers/library/position.py
@@ -13,9 +13,59 @@
 # Detect device by probing data address 0x75 - should return 0x68.
 
 import smbus2
+import struct
+
+REG_GYRO_CONFIG = 0x1b
+REG_ACCEL_CONFIG = 0x1c
+REG_PWR_MGMT_1 = 0x6b
+
+REG_ACCEL = 0x3b
+REG_TEMP = 0x41
+REG_GYRO = 0x43
 
 class Positioner:
 
+    def _read(self,addr):
+        return self.BUS.read_byte_data(self.ADDR,addr)
+    def _write(self,addr,value):
+        self.BUS.write_byte_data(self.ADDR,addr,value)
+    def _readmany(self,addr,count):
+        return self.BUS.read_i2c_block_data(self.ADDR,addr,count)
+
+    @property
+    def accel(self):
+        """Returns current raw accelerometer readings as [x,y,z]"""
+        raw = bytes(self._readmany(REG_ACCEL,6))
+        return struct.unpack(">hhh",raw)
+
+    @property
+    def gyro(self):
+        """Returns current raw gyroscope readings as [x,y,z]"""
+        raw = bytes(self._readmany(REG_GYRO,6))
+        return struct.unpack(">hhh",raw)
+
+    @property
+    def temp(self):
+        """Returns current raw temperature value"""
+        raw = bytes(self._readmany(REG_TEMP,2))
+        return struct.unpack(">h",raw)[0]
+    
+    @property
+    def temp_c(self):
+        """Returns current temperature converted to degrees Celsius"""
+        return self.temp / 340 + 36.53
+    
+    @property
+    def readings(self):
+        """Returns all sensor readings in one shot as a dictionary containing "accel", "gyro" and "temp"."""
+        raw = bytes(self._readmany(REG_ACCEL,12))
+        data = struct.unpack(">hhhhhhh",raw)
+        return {
+            "accel": (data[0],data[1],data[2]),
+            "gyro": (data[4],data[5],data[6]),
+            "temp": data[3]
+        }
+    
     def available(self):
 
         # try to read from address 0x75 of device
@@ -35,3 +85,15 @@ class Positioner:
         if not self.available():
             raise RuntimeError("I2C device not available.")
         
+        # set clock source to XGyro
+        self._write(REG_PWR_MGMT_1,0b01000001)
+
+        # set gyro range to 250
+        self._write(REG_GYRO_CONFIG,0b00000000)
+
+        # set accelerometer range to 2g
+        self._write(REG_ACCEL_CONFIG,0b00000000)
+
+        # wake up device
+        self._write(REG_PWR_MGMT_1,0b00000001)
+
