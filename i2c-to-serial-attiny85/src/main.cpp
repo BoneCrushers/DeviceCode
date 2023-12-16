13342fe6c49ea7fd566794c00222f352dfcc609f
fmillion <fmillion@gmail.com>
2023-12-16 04:06:50 -0600

    Move framing byte to after transmission

--- i2c-to-serial-attiny85/src/main.cpp
+++ i2c-to-serial-attiny85/src/main.cpp
@@ -12,12 +12,13 @@
  * 
  *  This code is intended to run on an ATtiny85 (or 45). It simply
  *  reads the MPU6050 accelerometer/gyroscope position module and
- *  transmits the read data over serial. 
- * 
+ *  transmits the read data over serial.
+ *
  *  Wiring:
  *  MPU6050 SCL -> PB2 (pin 7)
  *  MPU6050 SDA -> PB1 (pin 6)
- *  Serial      -> PB4 (pin 3)
+ *  Serial  TX  -> PB4 (pin 3) 
+ * 
  * 
  */
 
@@ -50,29 +51,39 @@ void setup() {
 }
 
 void startMpu6050() {
+
+  // read the ID byte from the chip
   TinyWireM.beginTransmission(MPU6050_ADDR);
   TinyWireM.write(MPU6050_PING);
   TinyWireM.endTransmission();
-
   TinyWireM.beginTransmission(MPU6050_ADDR);
   char verifyByte = TinyWireM.read();
   TinyWireM.endTransmission();
 
+  // if we did not get the correct ID byte, do nothing.
   if (verifyByte != 0x68);
     return;
   
   // write configuration bytes and start sensor
+  // these are hard-coded for this implementation for simplicity.
+
   // set clock source to XGyro
-  set(MPU6050_PWR_MGMT_1,0b01000001);
+  set( MPU6050_PWR_MGMT_1   , 0b01000001 );
 
   // set gyro range to 250
-  set(MPU6050_GYRO_CONFIG,0b00000000);
+  set( MPU6050_GYRO_CONFIG  , 0b00000000 );
 
   // set accelerometer range to 2g
-  set(MPU6050_ACCEL_CONFIG,0b00000000);
+  set( MPU6050_ACCEL_CONFIG , 0b00000000 );
 
   // wake up device
-  set(MPU6050_PWR_MGMT_1,0b00000001);
+  set( MPU6050_PWR_MGMT_1   , 0b00000001 );
+
+  // Write 0xFF as the opening framing byte
+  ser.write((uint8_t)0xFF);
+
+  // wait a bit for everything to settle
+  delay(500);
 
 }
 
@@ -106,13 +117,13 @@ void loop() {
     return;
   }
 
+  // Write the three data points from the MPU6050.
+  get_and_write( MPU6050_ACCEL , 6);
+  get_and_write( MPU6050_GYRO  , 6);
+  get_and_write( MPU6050_TEMP  , 2);
+
   // Write 0xFF as the framing byte
   ser.write((uint8_t)0xFF);
-
-  // Write the three data points from the MPU6050.
-  get_and_write(MPU6050_ACCEL,6);
-  get_and_write(MPU6050_GYRO,6);
-  get_and_write(MPU6050_TEMP,2);
 
   delay(100);
 
